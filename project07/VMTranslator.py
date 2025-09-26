ee804f4d5d428095f963f675641778ca6cde6c11
ikumasudo <44293565+ikumasudo@users.noreply.github.com>
2025-09-26 22:15:31 +0900

    VMTranslator

--- project07/VMTranslator.py
+++ project07/VMTranslator.py
@@ -1,27 +1,35 @@
 import argparse
+from parser import Parser
+from codeWriter import CodeWriter, Command
+from pathlib import Path
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Basic Hack Assembler")
     parser.add_argument("source", help=".vm file")
     args = parser.parse_args()
 
-    source_file: str = args.source
-    asm_file = source_file.replace(".vm", ".asm")
-    print(source_file, asm_file)
+    source: str = args.source
+    prog_name = source.replace(".vm", "")
 
-    # try:
-    #     with open(filename) as f:
-    #         lines = f.readlines()
+    try:
+        vm_stem = Path(source).stem
+        p = Parser(source_path=source)
+        w = CodeWriter(prog_name=prog_name)
+        while p.hasMoreLines():
+            p.advance()
+            if p.commandType() in (Command.C_PUSH, Command.C_POP):
+                c = f"{p.current_line:20}\t| {p.commandType():20} {p.arg1():10} {str(p.arg2()):10}"
+                print(c)
+                w.writeComment(c)
 
-    #     hack = Hack(lines)
-    #     bin_lines = hack.convert()
+                w.writePushPop(p.commandType(), p.arg1(), p.arg2())
+            else:
+                c = f"{p.current_line:20}\t| {p.commandType():20} {p.arg1():10}"
+                print(c)
+                w.writeComment(c)
 
-    #     with open(output_filename, "w", encoding="utf-8") as out:
-    #         out.write("\n".join(bin_lines))
-
-    # except FileNotFoundError:
-    #     print(f"エラー: 入力ファイルが見つかりません: {filename}")
-    # except ParsingError as e:
-    #     print(f"パースエラー: {e}")
-    # except Exception as e:
-    #     print(f"予期せぬエラーが発生しました: {e}")
+                w.writeArithmetic(p.arg1())
+    except Exception as e:
+        raise e
+    finally:
+        w.close()
